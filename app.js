0a15228377a9b6b77368fbd6718df76e1cbfe1ba
b4nkolE <obankole683@gmail.com>
2026-05-02 11:43:15 +0100

    extra

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import authRouter from "./routes/auth.routes.js";
 import inventoryRouter from "./routes/inventory.routes.js"
 import analyticsRouter from "./routes/analytics.routes.js"
 import userRouter from "./routes/users.routes.js"
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 
 
@@ -12,11 +14,14 @@ import userRouter from "./routes/users.routes.js"
 const app = express();
 app.use(express.json())
 
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 
 app.use("/api/v1/users", authRouter);
 app.use("/api/v1/inventory", inventoryRouter);
 app.use('/api/v1/analytics', analyticsRouter);
 app.use('/api/v1/users', userRouter)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 
